23e840520459f16ac3e1d7494c61d80d10b07259
emanuel benjumea <emanuel.benjumea01@uceva.edu.co>
2025-11-29 04:59:06 -0500

    logs

--- index.php
+++ index.php
@@ -37,6 +37,7 @@ spl_autoload_register(function ($class) {
 require_once __DIR__ . '/config/config.php';
 require_once __DIR__ . '/core/Router.php';
 require_once __DIR__ . '/core/Database.php';
+require_once __DIR__ . '/core/Logger.php';
 require_once __DIR__ . '/core/helpers.php';
 
 // Crear instancia del router
