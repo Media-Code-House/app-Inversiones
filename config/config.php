b6ab0b2729c4ed14d2b69fe4b83c97980fe4b48a
Brandon Quintero <121878425+branr9@users.noreply.github.com>
2025-12-20 15:59:27 -0500

    Revert "01 mapabran"

--- config/config.php
+++ config/config.php
@@ -20,19 +20,18 @@ define('DEBUG_MODE', !$isProduction);
 if ($isProduction) {
     // Configuración de producción (Hosting)
     define('DB_HOST', 'localhost');
-    define('DB_NAME', 'u418271893_developIvercio');
-    define('DB_USER', 'accesomchdevelop');
-    define('DB_PASS', 'Invermch.238#*Dev');
+    define('DB_NAME', 'u418271893_inversiones');
+    define('DB_USER', 'u418271893_santi');
+    define('DB_PASS', '3pX/yi>u3212321123123');
     define('DB_CHARSET', 'utf8mb4');
 } else {
     
     // Configuración de desarrollo (Local)
-     define('DB_HOST', 'localhost');
-    define('DB_NAME', 'db');
+    define('DB_HOST', 'localhost');
+    define('DB_NAME', 'sistema_lotes');
     define('DB_USER', 'root');
     define('DB_PASS', '');
     define('DB_CHARSET', 'utf8mb4');
-
 }
 
 // Configuración de sesión
