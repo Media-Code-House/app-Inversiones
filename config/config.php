577f5cca55b57bdb6b984c33752e8aec1484fa53
Media-Code-House <163322385+Media-Code-House@users.noreply.github.com>
2026-01-03 11:07:10 -0500

    Update config.php

--- config/config.php
+++ config/config.php
@@ -20,9 +20,9 @@ define('DEBUG_MODE', !$isProduction);
 if ($isProduction) {
     // Configuración de producción (Hosting)
     define('DB_HOST', '82.197.82.197');
-    define('DB_NAME', 'u418271893_developIvercio');
-    define('DB_USER', 'u418271893_accesomchdevel');
-    define('DB_PASS', 'Invermch.238#*Dev');
+    define('DB_NAME', 'u418271893_inversiones');
+    define('DB_USER', 'u418271893_santi');
+    define('DB_PASS', '3pX/yi>u3212321123123');
     define('DB_CHARSET', 'utf8mb4');
 } else {
     // Configuración de desarrollo (Local) - Conecta a la misma BD remota
