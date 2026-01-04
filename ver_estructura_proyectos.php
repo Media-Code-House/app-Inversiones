a5829b597222674f42e90cc6ccef1bdedbe324b5
Brandon Quintero <121878425+branr9@users.noreply.github.com>
2026-01-04 09:43:52 -0500

    actual

--- ver_estructura_proyectos.php
+++ ver_estructura_proyectos.php
@@ -1,1 +1,11 @@
+<?php
 require 'config/config.php';
+require 'core/Database.php';
+
+$db = Database::getInstance();
+
+echo "=== ESTRUCTURA DE LA TABLA proyectos ===\n\n";
+$columns = $db->fetchAll("SHOW COLUMNS FROM proyectos");
+foreach ($columns as $col) {
+    echo "{$col['Field']} - {$col['Type']} - {$col['Null']} - {$col['Key']}\n";
+}
